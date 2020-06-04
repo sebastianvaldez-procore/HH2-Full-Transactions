29f5e0b317f88f11e3490ab49998597e6028540e
sebastian valdez <sebastian.valdez@procore.com>
2020-06-04 14:54:00 -0700

    Use Job Guid and get all txns , sort headers and and write CSV

--- script.rb
+++ script.rb
@@ -4,6 +4,7 @@ require 'pry-byebug'
 require 'csv'
 require 'json'
 require 'tty-prompt'
+require 'date'
 
 # ******** SETUP ***********
 Dotenv.load()
@@ -40,15 +41,61 @@ hh2_auth_endpoint = '/WebServices/Authentication.svc'
 
 # log in to hh2
 hh2_login = HTTParty.post("#{@hh2_base_url}#{hh2_auth_endpoint}", headers: hh2_headers, body: hh2_body)
-if hh2_login.code == 200 && hh2_login.headers
-  @hh2_cookie = hh2_login.headers['set-cookie'] 
-else
-  raise StandardError.new("Error Logging into HH2\n base url: #{@hh2_base_url}|User: #{hh2_username}|pass: #{hh2_password}") if hh2_login.nil?
+@hh2_cookie = hh2_login.headers['set-cookie'] if hh2_login.code == 200 && hh2_login.headers
+
+def hh2_headers
+  {'Cookie' => @hh2_cookie}
 end
-puts "HH2 Login: Success! #{@hh2_cookie}\n"
 
+puts "HH2 Login: Success!\n"
 
+def full_job_transactions(job_guid:, version: 0 )
+  url = "#{@hh2_base_url}/JobCosting/Api/V1/JobTransaction.svc/job/transactions?job=#{job_guid}&version=#{version}"
+  puts "fetching job txns: #{url}"
+  HTTParty.get(url, headers: hh2_headers).parsed_response
+end
 
+guid = ''
+
+list = []
+res = full_job_transactions(job_guid: guid)
+list << res['ArrayOfJobTransaction']['JobTransaction']
+until res["ArrayOfJobTransaction"].nil? do
+  version = res['ArrayOfJobTransaction']['JobTransaction'].last['Version']
+  puts "version: #{version}"
+  res = full_job_transactions(job_guid: guid, version: version)
+  break if res["ArrayOfJobTransaction"].nil?
+  list << res['ArrayOfJobTransaction']['JobTransaction']
+end
+
+# map possible keys
+max_keys = {}
+list.flatten.map{|x| max_keys[x.keys.size.to_s] = x.keys if !max_keys[x.keys.size] }
+
+# all uniq keys
+headers = max_keys.reduce([]){|acc, x| acc << x[1] }.flatten.uniq.sort
+
+# method to clean up nils for 'N/A'
+def denilize(h)
+  h.each_with_object({}) { |(k,v),g|
+    g[k] = (Hash === v) ?  denilize(v) : v.nil? ? 'N/A' : v }
+end
+
+# extend data with full key set so CSV is neat
+temp = list.flatten.map do |data|
+  missing_keys = (headers - data.keys).reduce({}){|missing_hash, key| missing_hash.update(key => 'N/A') }
+  data.merge!(missing_keys)
+  denilize(data)
+  data.sort_by{|key, value| key}.to_h
+end
+
+# create CSV
+CSV.open("#{hh2_client_identifier}_#{Date.today.strftime}.csv", 'w', headers: true) do |csv|
+  csv << headers
+  temp.each do |row|
+      csv << row.values
+  end
+end
 
 puts 'transaction script'
 rescue => e
